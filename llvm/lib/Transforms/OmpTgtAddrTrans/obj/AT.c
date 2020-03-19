5e12e752a0e4f69aca1c528800245b94a0d4da2b
ponsheng <a7061120@gmail.com>
2020-03-19 14:09:11 +0800

    [llvm] Add bisear to OmpTgtAddrTrans

--- llvm/lib/Transforms/OmpTgtAddrTrans/obj/AT.c
+++ llvm/lib/Transforms/OmpTgtAddrTrans/obj/AT.c
@@ -1,6 +1,6 @@
 #include <stdint.h>
 
-struct RegionTy {
+struct ATTableTy {
   uintptr_t HstPtrBegin;
   uintptr_t HstPtrEnd;
   uintptr_t TgtPtrBegin;
@@ -8,15 +8,36 @@ struct RegionTy {
 };
 
 #pragma omp declare target
-void *AT(void* addr, struct RegionTy* table) {
+// Less mul/div ALU??
+/*void *AddrTrans(void* addr, struct ATTableTy* table) {
     int size = table[0].HstPtrBegin;
     uintptr_t ret = (uintptr_t) 0;
     uintptr_t addr_int = (intptr_t) addr;
     for (int i = 1; i <= size; i++) {
-        // TODO do binary search
         if (addr_int >= table[i].HstPtrBegin && addr_int < table[i].HstPtrEnd) {
             ret = addr_int - table[i].HstPtrBegin + table[i].TgtPtrBegin;
             break;
+        }
+    }
+    return (void*)ret;
+}*/
+// TODO Edge case, Compare perf use a switch on size
+// Binary search version
+void *AddrTrans(void* addr, struct ATTableTy* table) {
+    int size = table[0].HstPtrBegin;
+    uintptr_t ret = (uintptr_t) 0;
+    uintptr_t addr_int = (intptr_t) addr;
+    int head = 1, end = size + 1;
+    while (head < end) {
+        int mid = (head + end) << 1;
+        if (addr_int >= table[mid].HstPtrBegin) {
+            if (addr_int < table[mid].HstPtrEnd) {
+                ret = addr_int - table[mid].HstPtrBegin + table[mid].TgtPtrBegin;
+                break;
+            }
+            head = mid+1;
+        } else {
+            end = mid;
         }
     }
     return (void*)ret;

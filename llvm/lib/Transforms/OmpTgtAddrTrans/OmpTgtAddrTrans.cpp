e1455374b09b06757a0307f18b9d88530f06f224
ponsheng <a7061120@gmail.com>
2020-03-22 01:39:04 +0800

    [llvm] OmpTgtAddrTrans fix a lot of Inst
    Pass rodinia, backprop, kmeans

--- llvm/lib/Transforms/OmpTgtAddrTrans/OmpTgtAddrTrans.cpp
+++ llvm/lib/Transforms/OmpTgtAddrTrans/OmpTgtAddrTrans.cpp
@@ -17,6 +17,7 @@
 #include "llvm/Transforms/Utils/ValueMapper.h"
 #include "llvm/Analysis/CallGraph.h"
 #include "llvm/Transforms/IPO.h"
+#include "llvm/Analysis/OrderedInstructions.h"
 //#include "llvm/Analysis/MemoryDependenceAnalysis.h"
 
 using namespace llvm;
@@ -35,8 +36,10 @@ using namespace std;
 
 // define LLVM_MODULE if emit llvm module
 // otherwise, visible by llvm
-#define LLVM_MODULE
+//#define LLVM_MODULE
 
+
+// TODO What if it is a pointer data which will also be traced
 namespace {
   class OmpTgtAddrTrans : public ModulePass {
 
@@ -47,13 +50,20 @@ namespace {
 
     // Types
     IntegerType *IT;
-    StructType *ST;
-    PointerType *PT;
+    StructType *ATTableType;
+    PointerType *ATTablePtrType;
+    PointerType *AddrType;
+
+    Function *ATFunction;
 
     // llvm Module
     Module *module;
 
+    // UserList per Function
+    map<Function*,set<User*>> AllUserList;
  //   MemoryDependenceResults *MD;
+
+    unique_ptr<raw_fd_ostream> db_ostream;
 
     public:
     static char ID; // Pass identification, replacement for typeid
@@ -62,6 +72,17 @@ namespace {
 #ifndef LLVM_MODULE
       llvm::initializeOmpTgtAddrTransPass(*PassRegistry::getPassRegistry());
 #endif
+    }
+    raw_ostream &dp() {
+      std::error_code  EC;
+      static raw_fd_ostream null_ostream("/dev/null", EC, sys::fs::OF_None);
+      assert(!EC);
+      static char IsDP =(bool) getenv("DP2");
+      if (IsDP) {
+        return errs();
+      } else {
+        return null_ostream;
+      }
     }
     int8_t init(Module &M);
     Function *cloneFuncWithATArg(Function *F);
@@ -73,12 +94,17 @@ namespace {
     void getAnalysisUsage(AnalysisUsage &AU) const override;
     void traceArgInFunc(Function *, Argument*);
     bool isATFunction(Function *Func);
+    void insertATFuncBefore(Instruction *I, Value *Ptr, set<User*> &UserList);
+    unsigned getPtrDepth(Value *V);
+    unsigned getPtrDepth(Type *T);
+    DominatorTree &getDomTree(Function *F);
+    void getEntryFuncs(FunctionMapTy &EntryList);
   };
 }
 
 int8_t OmpTgtAddrTrans::init(Module &M) {
 
-  errs() << "OmpTgtAddrTransPass is called\n";
+  //errs() << "OmpTgtAddrTransPass is called\n";
   module = &M;
 
   // Use a metadata to avoid double application
@@ -90,6 +116,7 @@ int8_t OmpTgtAddrTrans::init(Module &M) {
   } else {
     M.getOrInsertNamedMetadata("omptgtaddrtrans");
   }
+
   // Create TableTy
   DataLayout DL(&M);
   vector<Type*> StructMem;
@@ -97,12 +124,24 @@ int8_t OmpTgtAddrTrans::init(Module &M) {
   for (int i = 0; i < 4; i++) {
     StructMem.push_back(IT);
   }
-  ST = StructType::create(M.getContext(), StructMem, "struct.ATTableTy", false);
-  PT = PointerType::getUnqual(ST);
+  ATTableType = StructType::create(M.getContext(), StructMem, "struct.ATTableTy", false);
+  ATTablePtrType = PointerType::getUnqual(ATTableType);
+
+  // Create function
+  AddrType = PointerType::get(IntegerType::get(M.getContext(), 8), 0);
+  vector<Type*> ParamTypes;
+  ParamTypes.push_back(AddrType);
+  ParamTypes.push_back(ATTablePtrType);
+  FunctionType *FT = FunctionType::get(AddrType, ParamTypes, false);
+  ATFunction = Function::Create(FT, GlobalValue::ExternalLinkage, "AddrTrans", M);
 
   // Get analysis
   //MD = &getAnalysis<MemoryDependenceWrapperPass>().getMemDep();
   return SUCCESS;
+}
+
+DominatorTree &OmpTgtAddrTrans::getDomTree(Function *F) {
+  return getAnalysis<DominatorTreeWrapperPass>(*F).getDomTree();
 }
 
 bool OmpTgtAddrTrans::isATFunction(Function *Func) {
@@ -112,6 +151,18 @@ bool OmpTgtAddrTrans::isATFunction(Function *Func) {
   return false;
 }
 
+unsigned OmpTgtAddrTrans::getPtrDepth(Value *V) {
+  return getPtrDepth(V->getType());
+}
+
+unsigned OmpTgtAddrTrans::getPtrDepth(Type *T) {
+  unsigned depth = 0;
+  while (PointerType *PT = dyn_cast<PointerType>(T)) {
+    depth++;
+    T = PT->getElementType();
+  }
+  return depth;
+}
 // Param
 // TODO kernel need to have some attribute nvvm annotation
 Function *OmpTgtAddrTrans::cloneFuncWithATArg(Function *F) {
@@ -125,7 +176,7 @@ Function *OmpTgtAddrTrans::cloneFuncWithATArg(Function *F) {
     //arg.dump();
     ArgsType.push_back(arg.getType());
   }
-  ArgsType.push_back(PT);
+  ArgsType.push_back(ATTablePtrType);
   FunctionType *FT = FunctionType::get(F->getReturnType(), ArgsType, false);
   Twine FuncName(F->getName());
 
@@ -149,70 +200,165 @@ Function *OmpTgtAddrTrans::cloneFuncWithATArg(Function *F) {
 
   // Clone body
   CloneFunctionInto(NewFunc, F, VMap, /*ModuleLevelChanges=*/true, Returns);
-  errs() << "cloneFunc " << F->getName() << " to " << NewFunc->getName() << "\n";
+  //errs() << "cloneFunc " << F->getName() << " to " << NewFunc->getName() << "\n";
 
   return NewFunc;
 }
 
 // Arg is CPU addr, keep trace it and insert AT function
 void OmpTgtAddrTrans::traceArgInFunc(Function *Func, Argument *Arg) {
-  queue<pair<Value*, int>> Vals;
-  set<User*> UserList;
+  // FIXME Possible that pointer to pointer is all GPU addr
+  struct PtrInfo {
+    Value *V;
+    unsigned PtrDepth;
+    Instruction *UseAfter;
+    PtrInfo(Value *V, unsigned D): V(V), PtrDepth(D), UseAfter(NULL) {};
+    PtrInfo(Value *V, unsigned D, Instruction *I): V(V), PtrDepth(D),
+      UseAfter(I) {};
+  };
+  //queue<pair<Value*, unsigned>> Vals;
+  queue<PtrInfo> Vals;
+  // TODO test per-function List
+
+  unsigned ArgDepth = getPtrDepth(Arg);
+
+  set<User*> &UserList = AllUserList[Func];
 
   if (!isATFunction(Func)) {
     errs() << "Tried to trace non-AT function: ";
     Func->getFunctionType()->dump();
     return;
   }
-  // TODO keep trace which Instruction has been traced
+  dp() << "traceArgInFunc: " << Func->getName() <<  " PtrDepth: "<< ArgDepth;
+  Arg->print(dp());
+  dp() << "\n";
 
-  // Check if this has been cloned
+  Vals.push({Arg, getPtrDepth(Arg)});
 
-  Vals.push({Arg,0});
+  unique_ptr<OrderedInstructions> OI =
+    make_unique<OrderedInstructions>(&getDomTree(Func));
+
   while (!Vals.empty()) {
-    Value *V = Vals.front().first;
-    int NestPtr = Vals.front().second;
+    PtrInfo Val = Vals.front();
+    Value *V = Val.V;
+    // TODO Change name to
+    unsigned NestPtr = Val.PtrDepth;
     Vals.pop();
+    if (!V) {
+      errs() << "Empty Value*: ";
+      continue;
+    }
+    dp()  << "Trace depth: " << NestPtr << " value: ";
+    V->print(dp());
+    dp() << "\n";
+
+    // Copy Use to avoid itr broken after insert and swap
+    list<Use*> CopiedUses;
     for (auto &Use : V->uses()) {
-      User *U = Use.getUser();
+      CopiedUses.push_back(&Use);
+    }
+
+    for (auto _U : CopiedUses) {
+      User *U = _U->getUser();
+      if (!U) {
+        errs() << "Empty User of Val: ";
+        V->dump();
+        continue;
+      }
+      // if UseAfter exist
+      if (Instruction *I = dyn_cast<Instruction>(U)) {
+        if (Val.UseAfter && OI->dfsBefore(I, Val.UseAfter)) {
+          // This is a use before it is important
+          // TODO  some use should be recheck if UseAfter is different
+          continue;
+        }
+      } else {
+        errs() << "!!Unknown user: func/Arg/Value/User/UseAfter: ";
+        errs() << Func->getName() << " ";
+        Arg->dump();
+        V->dump();
+        U->dump();
+        continue;
+      }
       // Check if User done before
       if (UserList.find(U) == UserList.end()) {
-        //U->dump();
+        U->print(dp());
+        dp() << "\n";
       } else {
         continue;
       }
       if (StoreInst *SI = dyn_cast<StoreInst>(U)) {
         if (SI->getPointerOperand() == dyn_cast<Value>(V)) {
-          assert(0 && "StoreInst to CPU address");
-          continue;
-        }
-        Vals.push({SI->getPointerOperand(), NestPtr + 1});
-      } else if (isa<LoadInst>(U)) {
-        if (NestPtr == 0) {
-          errs() << "!!!!!!!!!! Insert AT function here!!!!!!\n";
+          //assert(0 && "StoreInst to CPU address");
+          //continue;
+          if (NestPtr < ArgDepth) {
+            insertATFuncBefore(SI, V, UserList);
+            dp() << "!!!!!! Inserted AT function before Store\n";
+            // TODO replace further load
+          }
         } else {
+          // ptr is store into here
+          Vals.push({SI->getPointerOperand(), NestPtr + 1, SI});
+        }
+        // Only check use after this Store
+        // FIXME seems wierd
+      } else if (LoadInst *LI = dyn_cast<LoadInst>(U)) {
+        if (NestPtr < ArgDepth) {
+          insertATFuncBefore(LI, V, UserList);
+          dp() << "!!!!!! Inserted AT function before Load\n";
+        }
+        if (NestPtr > 1) {
           Vals.push({U, NestPtr - 1});
         }
       } else if (CallInst *CI = dyn_cast<CallInst>(U)) {
         // check if swap callinst
-        unsigned ArgIdx = Use.getOperandNo();
+        unsigned ArgIdx = _U->getOperandNo();
         Function *F = CI->getCalledFunction();
         if (!isATFunction(F)) {
-          // Cause redundant use?? TODO
+          // Cause redundant use?? FIXME
           CI = swapCallInst(CI);
           F = CI->getCalledFunction();
+          // FIXME push val???
+        }
+        if (NestPtr < ArgDepth) {
+          dp() << "!!!!!! Inserted AT function before call\n";
+          insertATFuncBefore(CI, V, UserList);
         }
         traceArgInFunc(F, F->arg_begin() + ArgIdx);
+        continue;
+        // dont push call to Vals
       } else if (GetElementPtrInst *GEPI = dyn_cast<GetElementPtrInst>(U)) {
         if (GEPI->getPointerOperand() == dyn_cast<Value>(V)) {
           Vals.push({U, NestPtr});
         }
-      } else if (Instruction *Inst = dyn_cast<Instruction>(U)) {
-        errs() << "Unknown inst\n";
-        Inst->dump();
-        continue;
+      } else if (BitCastInst *BCI = dyn_cast<BitCastInst>(U)) {
+        if (getPtrDepth(BCI->getSrcTy()) == getPtrDepth(BCI->getDestTy())) {
+          Vals.push({U, NestPtr});
+        } else {
+          errs() << "Ignore different depth BitCastInst for now: ";
+          BCI->dump();
+        }
+      } else if (AtomicRMWInst *AI = dyn_cast<AtomicRMWInst> (U)) {
+        if (AI->getPointerOperand() == dyn_cast<Value>(V)) {
+          if (NestPtr < ArgDepth) {
+            insertATFuncBefore(AI, V, UserList);
+            dp() << "!!!!!! Inserted AT function before AtomicRMWInst\n";
+          }
+        }
+        // TODO  trace??  in else
+      } else if (AtomicCmpXchgInst *ACXI = dyn_cast<AtomicCmpXchgInst>(U)) {
+        if (ACXI->getPointerOperand() == dyn_cast<Value>(V)) {
+          if (NestPtr < ArgDepth) {
+            insertATFuncBefore(ACXI, V, UserList);
+            dp() << "!!!!!! Inserted AT function before AtomicCmpXchgInst\n";
+          }
+        }
+        // TODO  trace??  in else
       } else {
-        errs() << "Unknown use\n";
+        errs() << "!!Unknown Inst: func/Arg/Value/User: ";
+        errs() << Func->getName() << " ";
+        Arg->dump();
+        V->dump();
         U->dump();
         continue;
       }
@@ -238,6 +384,48 @@ void OmpTgtAddrTrans::traceArgInFunc(Function *Func, Argument *Arg) {
     }
   }
   */
+}
+
+// Inst has to be in AT function
+// TODO Load only once for same addr
+//void OmpTgtAddrTrans::insertATFuncBefore(Instruction *Inst, Use *PtrUse, set<User*> &UserList) {
+void OmpTgtAddrTrans::insertATFuncBefore(Instruction *Inst, Value *PtrAddr, set<User*> &UserList) {
+  Value *ATTableArg = Inst->getFunction()->arg_end() - 1;
+
+  /*
+  if (LoadInst *LI = dyn_cast<LoadInst>(Inst)) {
+    PtrAddr = LI->getPointerOperand();
+  } else if (StoreInst *SI = dyn_cast<StoreInst>(Inst)) {
+    PtrAddr = SI->getPointerOperand();
+  } else if (CallInst *CI = dyn_cast<CastInst>(Inst)) {
+    // TODO
+    PtrAddr = SI->getPointerOperand();
+  } else {
+    errs() << "insertATFuncBefore on unsupported inst\n";
+    return;
+  }*/
+
+  dp () << "insertATFuncBefore PtrAddr: " ;
+  PtrAddr->print(dp());
+  dp() << "\n";
+  // insert bitcast
+  CastInst *PreCastI = CastInst::Create(Instruction::BitCast, PtrAddr,
+      AddrType, "PreATCast", Inst);
+
+  // insert call
+  vector<Value*> Args;
+  Args.push_back(PreCastI);
+  Args.push_back(ATTableArg);
+  CallInst *CI = CallInst::Create(ATFunction->getFunctionType(), ATFunction,
+      Args, "TransResult", Inst);
+  // insert bitcast
+  CastInst *PostCastI = CastInst::Create(Instruction::BitCast, CI,
+      PtrAddr->getType(), "PostATCast", Inst);
+  //Inst->replaceUsesOfWith (PtrUse->get(), PostCastI);
+  Inst->replaceUsesOfWith (PtrAddr, PostCastI);
+  UserList.insert(PreCastI);
+  UserList.insert(CI);
+  UserList.insert(PostCastI);
 }
 
 // Recursive
@@ -310,8 +498,9 @@ CallInst *OmpTgtAddrTrans::swapCallInst(CallInst *CI) {
 
   // Create new inst
   CallInst *CINew = NULL;
-  CINew = CallInst::Create(NewCallee->getFunctionType(), NewCallee, ArgsOfNew);
-  CINew->insertBefore(CI);
+  CINew = CallInst::Create(NewCallee->getFunctionType(), NewCallee, ArgsOfNew,
+      Twine::createNull(), CI);
+  //CINew->insertBefore(CI);
   CI->replaceAllUsesWith(CINew);
   CI->dropAllReferences();
   CI->eraseFromParent ();
@@ -339,6 +528,47 @@ void OmpTgtAddrTrans::eraseFunction(FunctionMapTy FunctionTrans, Function* F) {
   F->eraseFromParent();
 }
 
+void OmpTgtAddrTrans::getEntryFuncs(FunctionMapTy &EntryList) {
+  NamedMDNode *NVVM = module->getNamedMetadata("nvvm.annotations");
+
+  for (const auto &MD : NVVM->operands()) {
+    if (MD->getNumOperands() != 3) {
+      continue;
+    }
+    Function *Entry;
+    if (!MD->getOperand(0).get()) {
+      continue;
+    }
+    if (auto *VAM = dyn_cast<ValueAsMetadata>(MD->getOperand(0).get())) {
+      if (Function *F = dyn_cast<Function>(VAM->getValue())) {
+        Entry = F;
+        goto SECOND;
+      }
+    }
+    continue;
+SECOND:
+    if (!MD->getOperand(1).get()) {
+      continue;
+    }
+    if (auto *MDS = dyn_cast<MDString>(MD->getOperand(1).get())) {
+      if (MDS->getString().compare("kernel") == 0) {
+        goto THIRD;
+      }
+    }
+    continue;
+THIRD:
+    if (!MD->getOperand(2).get()) {
+      continue;
+    }
+    if (auto *CAM = dyn_cast<ConstantAsMetadata>(MD->getOperand(2).get())) {
+      if (CAM->getValue()->isOneValue()) {
+        EntryList[Entry] = NULL;
+        dp() << "Get entry Func: " << Entry->getName() << "\n";
+      }
+    }
+  }
+}
+
 bool OmpTgtAddrTrans::runOnModule(Module &M) {
   bool changed = false;
 
@@ -346,15 +576,8 @@ bool OmpTgtAddrTrans::runOnModule(Module &M) {
     return changed;
   }
 
-  // Get entry funcs
-  string EntryFuncName("__omp_offloading_");
-  for (auto &F : M.functions()) {
-    if (F.getName().contains(EntryFuncName)) {
-      FunctionTransEntry[&F] = NULL;
-      errs() << "Capture entry function: " << F.getName() << "\n";
-      continue;
-    }
-  }
+  // Get entry funcs from metadata
+  getEntryFuncs(FunctionTransEntry);
 
   if (FunctionTransEntry.size()) {
     changed = true;
@@ -376,10 +599,8 @@ bool OmpTgtAddrTrans::runOnModule(Module &M) {
     auto ArgItr = F->arg_begin();
     // All pointer args of entry function is CPU address
     for (size_t i = 0; i < EntryArgSize; i++, ArgItr++) {
-      if (auto PT = dyn_cast<PointerType> (ArgItr->getType())) {
-        if (PT->getElementType()->isPointerTy()) {
-          traceArgInFunc(F, ArgItr);
-        }
+      if (getPtrDepth(ArgItr) > 1) {
+        traceArgInFunc(F, ArgItr);
       }
     }
   }
@@ -390,7 +611,7 @@ bool OmpTgtAddrTrans::runOnModule(Module &M) {
 void OmpTgtAddrTrans::getAnalysisUsage(AnalysisUsage &AU) const {
   AU.setPreservesCFG();
 //  AU.addRequired<MemoryDependenceWrapperPass>();
-//  AU.addRequired<DominatorTreeWrapperPass>();
+  AU.addRequired<DominatorTreeWrapperPass>();
 //  AU.addRequired<AAResultsWrapperPass>();
 //  AU.addRequired<LoopInfoWrapperPass>();
 //  AU.addRequired<DominatorTreeWrapperPass>();
@@ -411,6 +632,7 @@ ModulePass *llvm::createOmpTgtAddrTransPass() {
 }
 #endif
 
+// TODO
 //INITIALIZE_PASS_BEGIN(OmpTgtAddrTrans, "OmpTgtAddrTransPass", "Description TODO", false, false)
 //INITIALIZE_PASS_DEPENDENCY(CallGraphWrapperPass)
 //INITIALIZE_PASS_DEPENDENCY(DominatorTreeWrapperPass)
